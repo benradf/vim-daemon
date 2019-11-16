fec8680e07db35e5236e1dbda1492ea160eeeae6
Ben Radford <benradf@users.noreply.github.com>
2019-11-16 10:22:02 +0000

    Add extract and peek Stream functions.

--- Stream.hs
+++ Stream.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Stream where
+module Stream
+  ( Stream
+  , extract
+  , fromList
+  , peek
+  ) where
 
 
 data Stream m a
@@ -8,8 +13,17 @@ data Stream m a
   | EndOfStream
 
 
+extract :: Applicative m => Stream m a -> m (Maybe (a, Stream m a))
+extract = \case
+  Stream x xs -> Just <$> ((,) <$> x <*> pure xs)
+  EndOfStream -> pure Nothing
+
+peek :: Applicative m => Stream m a -> m (Maybe a)
+peek = \case
+  Stream x _ -> Just <$> x
+  EndOfStream -> pure Nothing
+
 fromList :: Applicative m => [a] -> Stream m a
 fromList = \case
   x : xs -> Stream (pure x) (fromList xs)
   [] -> EndOfStream
-
